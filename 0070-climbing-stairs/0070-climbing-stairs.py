cc142644e045d5b00c9b7bb6e754cd0b965b38b2
Kinshuk Goyal <72309507+Kinshuk2003@users.noreply.github.com>
2022-12-12 11:07:26 +0530

    Time: 66 ms (9.87%), Space: 13.8 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,9 +3,10 @@ class Solution:
         dp=[1,2]
         if n<=3:
             return n
-        
+        way=0
         for i in range(2,n):
             dp.append(dp[i-2]+dp[i-1])
+            
         
         return dp[n-1]
         
